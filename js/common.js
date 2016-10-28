a39920ab4a3dd92165e971ee2449e755eb3c8a54
João Magalhães <joaopbgmagalhaes@gmail.com>
2016-10-28 13:50:13 +0100

    Add application structure

---
+++ js/common.js
@@ -0,0 +1,10 @@
+/*global define*/
+'use strict';
+
+define([], function () {
+    return {
+        sendAuthentication: function(xhr) {
+            xhr.setRequestHeader('Client-ID', 'q9k2mh6i7wxofogt73wr69eihx41ft7');
+        },
+    };
+});
